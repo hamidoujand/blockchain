4bfa4c5e73fef01bb368225860d87ad5766685b4
Hamid Oujand <hmdrzadev@gmail.com>
2024-10-21 19:57:23 +0330

    add support to add transaction into mempool.

--- app/services/node/handlers/v1/public/public.go
+++ app/services/node/handlers/v1/public/public.go
@@ -3,8 +3,10 @@ package public
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/hamidoujand/blockchain/business/web/errs"
 	"github.com/hamidoujand/blockchain/foundation/blockchain/database"
 	"github.com/hamidoujand/blockchain/foundation/blockchain/state"
 	"github.com/hamidoujand/blockchain/foundation/web"
@@ -74,4 +76,35 @@ func (h Handlers) Mempool(ctx context.Context, w http.ResponseWriter, r *http.Re
 	}
 
 	return web.Respond(ctx, w, trans, http.StatusOK)
+}
+
+// SubmitWalletTransaction adds new transactions to the mempool.
+func (h Handlers) SubmitWalletTransaction(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	// Decode the JSON in the post call into a Signed transaction.
+	var signedTx database.SignedTx
+	if err := web.Decode(r, &signedTx); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	h.Log.Infow("add tran", "traceid", v.TraceID, "sig:nonce", signedTx, "from", signedTx.FromID, "to", signedTx.ToID, "value", signedTx.Value, "tip", signedTx.Tip)
+
+	// Ask the state package to add this transaction to the mempool. Only the
+	// checks are the transaction signature and the recipient account format.
+	// It's up to the wallet to make sure the account has a proper balance and
+	// nonce. Fees will be taken if this transaction is mined into a block.
+	if err := h.State.UpsertWalletTransaction(signedTx); err != nil {
+		return errs.NewTrusted(err, http.StatusBadRequest)
+	}
+	resp := struct {
+		Status string `json:"status"`
+	}{
+		Status: "transactions added to mempool",
+	}
+
+	return web.Respond(ctx, w, resp, http.StatusOK)
 }
