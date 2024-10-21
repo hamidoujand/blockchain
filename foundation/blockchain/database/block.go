6c0f2c28e5faae625a6ddf93df75d9d78e6ba505
Hamid Oujand <hmdrzadev@gmail.com>
2024-10-22 01:31:07 +0330

    add merkle tree support

---
+++ foundation/blockchain/database/block.go
@@ -0,0 +1,77 @@
+package database
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/hamidoujand/blockchain/foundation/blockchain/merkle"
+)
+
+// BlockHeader represents common information required for each block.
+type BlockHeader struct {
+	Number        uint64    `json:"number"`          // Ethereum: Block number in the chain.
+	PrevBlockHash string    `json:"prev_block_hash"` // Bitcoin: Hash of the previous block in the chain.
+	TimeStamp     uint64    `json:"timestamp"`       // Bitcoin: Time the block was mined.
+	BeneficiaryID AccountID `json:"beneficiary"`     // Ethereum: The account who is receiving fees and tips.
+	Difficulty    uint16    `json:"difficulty"`      // Ethereum: Number of 0's needed to solve the hash solution.
+	MiningReward  uint64    `json:"mining_reward"`   // Ethereum: The reward for mining this block.
+	StateRoot     string    `json:"state_root"`      // Ethereum: Represents a hash of the accounts and their balances.
+	TransRoot     string    `json:"trans_root"`      // Both: Represents the merkle tree root hash for the transactions in this block.
+	Nonce         uint64    `json:"nonce"`           // Both: Value identified to solve the hash solution.
+}
+
+// Block represents a group of transactions batched together.
+type Block struct {
+	Header     BlockHeader
+	MerkleTree *merkle.Tree[BlockTx]
+}
+
+// BlockData represents what can be serialized to disk and over the network.
+type BlockData struct {
+	Hash   string      `json:"hash"`
+	Header BlockHeader `json:"block"`
+	Trans  []BlockTx   `json:"trans"`
+}
+
+// ToBlock converts a storage block into a database block.
+func ToBlock(blockData BlockData) (Block, error) {
+	tree, err := merkle.NewTree(blockData.Trans)
+	if err != nil {
+		return Block{}, err
+	}
+
+	block := Block{
+		Header:     blockData.Header,
+		MerkleTree: tree,
+	}
+
+	return block, nil
+}
+
+// Hash returns the unique hash for the Block.
+func (b Block) Hash() string {
+	if b.Header.Number == 0 {
+		return ZeroHash
+	}
+
+	// CORE NOTE: Hashing the block header and not the whole block so the blockchain
+	// can be cryptographically checked by only needing block headers and not full
+	// blocks with the transaction data. This will support the ability to have pruned
+	// nodes and light clients in the future.
+	// - A pruned node stores all the block headers, but only a small number of full
+	//   blocks (maybe the last 1000 blocks). This allows for full cryptographic
+	//   validation of blocks and transactions without all the extra storage.
+	// - A light client keeps block headers and just enough sufficient information
+	//   to follow the latest set of blocks being produced. The do not validate
+	//   blocks, but can prove a transaction is in a block.
+	// Hash returns a unique string for the value.
+
+	data, err := json.Marshal(b.Header) //hash only the header
+	if err != nil {
+		return ZeroHash
+	}
+
+	hash := sha256.Sum256(data)
+	return hexutil.Encode(hash[:])
+}
